2a715f48385d468402e5ef669ee3bc29de3ba359
Az <103241502+HereLiesAz@users.noreply.github.com>
2026-09-07 20:57:48 -0500

    Check for HG2Gui updates automatically at startup

--- composeApp/src/main/java/com/hereliesaz/hg2gui/util/GenericFileProvider.java
+++ composeApp/src/main/java/com/hereliesaz/hg2gui/util/GenericFileProvider.java
@@ -3,6 +3,7 @@ package com.hereliesaz.hg2gui.util;
 import androidx.core.content.FileProvider;
 
 import com.hereliesaz.hg2gui.terminal.TermuxRuntimeRepair;
+import com.hereliesaz.hg2gui.update.AppUpdateChecker;
 
 public class GenericFileProvider extends FileProvider {
     public static final String PROVIDER_NAME = "com.hereliesaz.hg2gui" + ".FILE_PROVIDER";
@@ -12,6 +13,7 @@ public class GenericFileProvider extends FileProvider {
         boolean created = super.onCreate();
         if (getContext() != null) {
             TermuxRuntimeRepair.INSTANCE.repair(getContext());
+            AppUpdateChecker.INSTANCE.checkAndNotify(getContext());
         }
         return created;
     }
